3aaa862ba536974235f18948f33a03c003fbb73c
Dan Shuman <dan.13.shuman@gmail.com>
2015-08-19 18:46:36 -0500

    Removes unnecessary files

--- cligp/test.py
+++
@@ -1,5 +0,0 @@
-vs = input("Enter the variables corresponding to the columns in the data \
-           file, in order from left to right, separated only by commas: ")
-print(vs)
-v = vs.split(",")
-print(v)
