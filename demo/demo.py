af9ca33c091113028adb20dd840a4db3103632e5
Dan Shuman <dan.13.shuman@gmail.com>
2015-08-23 20:39:56 -0500

    Updates documentation

--- demo/demo.py
+++ demo/demo.py
@@ -7,8 +7,11 @@ from copy import deepcopy
 """Prepatory steps"""
 
 
-"""Steps 1 & 2
-Specify the function and terminal sets.
+"""Steps 1 & 2: Specify the function and terminal sets
+
+The function and terminal sets, together known as the primitive set, specify the
+components which can be used to construct possible solutions to the problem. For
+a mathematical problem, these will be mathematical functions and constants.
 """
 pset = primitives.pset
 v = ["x"]
@@ -17,17 +20,21 @@ for item in v:
 s = tools.primitive_handler(pset, v)
 
 
-"""Step 3
-Define the fitness measure. This is the pygp.fitness function found
-in the pygp module; the data this fitness function will use to evaluate evolved
-programs and determine their fitnesses is imported below.
+"""Step 3: Define the fitness measure
+
+The fitness measure is the pygp.fitness function found in the pygp module; the
+data this fitness function will use to evaluate evolved programs and determine
+their fitnesses is imported below.
 """
 filename = "datafile.csv"
 data = tools.read_data(filename)
 
 
-"""Step 4
-Set run parameters.
+"""Step 4: Set run parameters
+
+These controls determine the size of the population, maximum starting
+depth of trees, the number of individuals selected to participate in tournament
+selection, and the rates of different recombination and mutation operations.
 """
 popsize = 1000
 max_depth = 3
@@ -37,18 +44,23 @@ mut_rate = 1.0
 tourn_size = 5
 
 
-"""Step 5
-Specify termination condition.
+"""Step 5: Specify termination condition
+
+If it is known that the exact solution to the problem can be represented using
+the function and terminal sets, the target fitness can be arbitrarily low, but
+in general this cannot be guaranteed, and so it may be beneficial not to
+restrict solutions with an extremely small target fitness.
 """
-target_fitness = 0.00001
+target_fitness = 0.1
 
 
 """Running GP"""
 
 
 """Initialization
+
 An initial population is generated, in this case using the ramped half-and-half
-technique.
+technique. This ensures a variety of program sizes and structures.
 """
 def ramped(popsize, pset, s, max_depth):
     """Initializes and returns a population using the ramped half-and-half
@@ -60,7 +72,7 @@ def ramped(popsize, pset, s, max_depth):
     half = int(popsize / 2)
     for i in range(1, half):
         pop.append(pygp.BinaryTree(pset, s, "full", randint(1, max_depth)))
-        
+
     for i in range(half, popsize+1):
         pop.append(pygp.BinaryTree(pset, s, "grow", randint(1, max_depth)))
 
@@ -71,6 +83,7 @@ pop = ramped(popsize, pset, s, max_depth)
 
 
 """Evolve the population toward a solution
+
 Continue evolving the population until an individual
 meeting the target fitness is found.
 """
@@ -84,7 +97,7 @@ def evolve(pop, generation=1):
     """
     print(generation)
     best_in_gen = pygp.termination_test(pop, data) # include a program return
-    
+
     if best_in_gen[1] < target_fitness:
         return {"best":best_in_gen[0], "score":best_in_gen[1],
                 "gen": generation}
